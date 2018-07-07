afd098db4806a3c9c699c12270e6efd12757d8ee
Arebis <michal.sibera@windowslive.com>
2018-07-08 00:09:31 +0200

    Created sample with two drag and drop lists

--- ARDroneBlockProgramming.Web/ClientApp/components/Home/Home.vue
+++ ARDroneBlockProgramming.Web/ClientApp/components/Home/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <h1>Home</h1>
-        <draggable>
-
+        <draggable v-model="listA" style="margin-bottom: 50px" :options="{ group: 'sample' }">
+            <div v-for="element in listA" :key="element.id">{{ element.name }}</div>
+        </draggable>
+        <draggable v-model="listB" :options="{ group: 'sample' }">
+            <div v-for="element in listB" :key="element.id">{{ element.name }}</div>
         </draggable>
     </div>
 </template>
@@ -10,18 +13,26 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import draggable from './../types/vuedraggable';
+import draggable from 'vuedraggable';
 
 
-@Component
-export default class Home extends Vue {
-    @Prop() testArr: Array<object>;
-
-    constructor() {
-        super();
-        this.testArr = [
-        ];
+@Component({
+    components: {
+        draggable
     }
+})
+export default class Home extends Vue {
+
+    listA: Array<object> = [
+            { id: 1, name: 'some'},
+            { id: 2, name: 'more'},
+            { id: 3, name: 'samples'},
+        ];
+
+    listB: Array<object> = [
+            { id: 4, name: 'other'},
+            { id: 5, name: 'examples'},
+        ];
 }
 </script>
 
