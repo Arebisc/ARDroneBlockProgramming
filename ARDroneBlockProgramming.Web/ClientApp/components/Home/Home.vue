8f3f2f89d94e39735269d413c4b0a75e1ac525cb
Arebis <michal.sibera@windowslive.com>
2018-07-07 23:05:54 +0200

    Moved Home component to directory

--- ARDroneBlockProgramming.Web/ClientApp/components/Home/Home.vue
+++ ARDroneBlockProgramming.Web/ClientApp/components/Home/Home.vue
@@ -1,17 +1,27 @@
 <template>
     <div>
         <h1>Home</h1>
+        <draggable>
+
+        </draggable>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 import draggable from './../types/vuedraggable';
 
 
 @Component
 export default class Home extends Vue {
+    @Prop() testArr: Array<object>;
+
+    constructor() {
+        super();
+        this.testArr = [
+        ];
+    }
 }
 </script>
 
