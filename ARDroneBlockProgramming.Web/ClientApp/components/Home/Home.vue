efe1ef1d4b3473975aac9be815e14d6f27e99f48
Arebis <michal.sibera@windowslive.com>
2018-07-08 20:16:23 +0200

    Styling

--- ARDroneBlockProgramming.Web/ClientApp/components/Home/Home.vue
+++ ARDroneBlockProgramming.Web/ClientApp/components/Home/Home.vue
@@ -1,16 +1,21 @@
 <template>
     <v-layout row>
-        <v-flex xs4>
+        <v-flex xs4 class="actions-wrapper">
             <h1>Akcje</h1>
-            <draggable v-model="droneActions" style="margin-bottom: 50px" :options="{ group: { name: 'drone-actions-group', pull:'clone' } }" class="actions-container">
+            <draggable v-model="droneActions" style="margin-bottom: 50px" :options="{ 
+                group: { name: 'drone-actions-group', pull:'clone', put:false }, sort: false }"
+                class="actions-container">
                 <div v-for="(element, index) in droneActions" :key="index" class="action-tile">{{ element.actionLabel }}</div>
             </draggable>
         </v-flex>
         <v-flex xs4>
             <h1>Blok instrukcji do wykonania</h1>
-            <draggable v-model="userDefinedActions" :options="{ group: 'drone-actions-group' }" class="user-defined-actions">
-                <div v-for="(element, index) in userDefinedActions" :key="index">{{ element.actionLabel }}</div>
+            <h3>Start</h3>
+            <draggable v-model="userDefinedActions" :options="{ group: 'drone-actions-group' }" class="user-defined-actions-contaner">
+                <div v-for="(element, index) in userDefinedActions" :key="index" class="action-tile">{{ element.actionLabel }}</div>
             </draggable>
+            <p class="text-center">Umieszczaj akcje powyżej</p>
+            <h3>Koniec</h3>
         </v-flex>
     </v-layout>
 </template>
@@ -39,19 +44,29 @@ export default class Home extends Vue {
 }
 </script>
 
-<style>
+<style scoped>
+.actions-wrapper {
+    margin-right: 15px;
+}
+
 .actions-containter {
     width: 100%;
+    background: #ffffff;
+    margin: 5px;
+    padding: 5px;
 }
 
 .action-tile {
     width: 100%;
-    background: #ffffff;
+    background: #eaeaea;
     height: 50px;
     font-size: 18px;
+    padding: 5px;
 }
 
-.user-defined-actions {
+.user-defined-actions-contaner {
     min-height: 50px;
+    background: #ffffff;
+    padding-bottom: 50px;
 }
 </style>
