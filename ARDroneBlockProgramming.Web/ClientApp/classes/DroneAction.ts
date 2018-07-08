54a9582e9c89ed45be84034fd71b260d2a7c85a6
Arebis <michal.sibera@windowslive.com>
2018-07-08 13:18:16 +0200

    Renamed DroneCommand to DroneAction

---
+++ ARDroneBlockProgramming.Web/ClientApp/classes/DroneAction.ts
@@ -0,0 +1,7 @@
+class DroneAction {
+    public actionLabel: string;
+
+    public constructor(actionLabel: string) {
+        this.actionLabel = actionLabel;
+    }
+}
--- ARDroneBlockProgramming.Web/ClientApp/classes/DroneCommand.ts
+++
@@ -1,7 +0,0 @@
-class DroneCommand {
-    public commandLabel: string;
-
-    public constructor(commandLabel: string) {
-        this.commandLabel = commandLabel;
-    }
-}
