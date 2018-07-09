6d3fd877542fdedafc9aa1f5e65152bdae93b53c
Arebis <michal.sibera@windowslive.com>
2018-07-09 19:46:00 +0200

    Added actions properties

--- ARDroneBlockProgramming.Web/ClientApp/components/Home/ActionTile.vue
+++ ARDroneBlockProgramming.Web/ClientApp/components/Home/ActionTile.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="action-tile">{{ action.actionLabel }}</div>
+    <v-layout row wrap class="action-tile">
+        <v-flex xs6>
+            {{ droneAction.actionLabel }}
+        </v-flex>
+        <v-flex xs6>
+            <v-icon>remove_circle</v-icon>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script lang="ts">
@@ -11,11 +18,11 @@ import { DroneAction } from '../../classes/DroneAction';
 
 @Component({
     props: {
-        action: DroneAction
+        droneAction: DroneAction
     }
 })
 export default class Home extends Vue {
-
+    
 }
 </script>
 
@@ -23,8 +30,7 @@ export default class Home extends Vue {
 .action-tile {
     width: 100%;
     background: #f5f3f3;
-    height: 50px;
     font-size: 18px;
-    padding: 5px;
+    padding: 10px 5px 5px 10px;
 }
 </style>
