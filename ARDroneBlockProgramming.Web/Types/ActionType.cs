be822ad7327d8f927f7fc175661e21a84461ed43
Arebis <michal.sibera@windowslive.com>
2018-07-19 23:32:44 +0200

    Added ActionType in API

---
+++ ARDroneBlockProgramming.Web/Types/ActionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARDroneBlockProgramming.Web.Types
+{
+    public enum ActionType
+    {
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3,
+        Forward = 4,
+        Back = 5
+    }
+}
