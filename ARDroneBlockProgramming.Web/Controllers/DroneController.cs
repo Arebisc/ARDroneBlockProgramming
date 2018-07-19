a6cb409f2a27f8b2645a3e5209dc8c7cc7cbac99
Arebis <michal.sibera@windowslive.com>
2018-07-19 23:33:32 +0200

    Added DroneController with Post Action to receive userDefined actions

---
+++ ARDroneBlockProgramming.Web/Controllers/DroneController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ARDroneBlockProgramming.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARDroneBlockProgramming.Web.Controllers
+{
+    [Route("/api/Drone")]
+    public class DroneController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index([FromBody]DroneActionViewModel[] droneActionViewModel)
+        {
+            return Ok(droneActionViewModel);
+        }
+    }
+}
