180b56576aaf00b3683ed09a00e67eac1034ec20
Arebis <michal.sibera@windowslive.com>
2018-07-19 23:33:06 +0200

    Added DroneActionViewModel in API

---
+++ ARDroneBlockProgramming.Web/ViewModels/DroneActionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ARDroneBlockProgramming.Web.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARDroneBlockProgramming.Web.ViewModels
+{
+    public class DroneActionViewModel
+    {
+        public string ActionLabel { get; set; }
+        public ActionType ActionType { get; set; }
+        public double Speed { get; set; }
+        public int Duration { get; set; }
+    }
+}
