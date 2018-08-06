564ebdaa02379c0e26aa95caff6eda755d758e2d
Arebis <michal.sibera@windowslive.com>
2018-08-06 20:12:00 +0200

    Added request from ASP to drone API

--- ARDroneBlockProgramming.Web/Controllers/DroneController.cs
+++ ARDroneBlockProgramming.Web/Controllers/DroneController.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using ARDroneBlockProgramming.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ARDroneBlockProgramming.Web.Controllers
 {
     [Route("/api/Drone")]
     public class DroneController : Controller
     {
+        private const string _droneEndpoint = "http://localhost:3000";
+
         [HttpPost]
-        public IActionResult Index([FromBody]DroneActionViewModel[] droneActionViewModel)
+        public async Task<IActionResult> Index([FromBody]DroneActionViewModel[] droneActionViewModel)
         {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    _droneEndpoint,
+                     new StringContent(JsonConvert.SerializeObject(droneActionViewModel), Encoding.UTF8, "application/json"));
+            }
+
             return Ok(droneActionViewModel);
         }
     }
