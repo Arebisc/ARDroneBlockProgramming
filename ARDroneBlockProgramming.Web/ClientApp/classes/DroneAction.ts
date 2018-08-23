725640e8521a1c50611029895a75ed65c156e877
Arebis <michal.sibera@windowslive.com>
2018-08-23 18:36:38 +0200

    Updated DroneAction class to contain tag

--- ARDroneBlockProgramming.Web/ClientApp/classes/DroneAction.ts
+++ ARDroneBlockProgramming.Web/ClientApp/classes/DroneAction.ts
@@ -1,4 +1,4 @@
-import { ActionType } from '../Types/ActionType';
+import { ActionType } from '../types/ActionType';
 
 
 export class DroneAction {
@@ -6,17 +6,20 @@ export class DroneAction {
     public actionType: ActionType;
     public speed: number | null;
     public duration: number | null;
+    public tag: string | null;
 
     public constructor(
         actionLabel: string,
         actionType: ActionType,
         speed: number | null = null,
-        duration: number | null = null
+        duration: number | null = null,
+        tag: string | null = null
     ) {
         this.actionLabel = actionLabel;
         this.actionType = actionType;
         this.speed = 0.1;
         this.duration = 1000;
+        this.tag = tag;
 
         if(speed != null){
             this.speed = speed;
