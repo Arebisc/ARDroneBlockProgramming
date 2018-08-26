b13a7199ab8d911006e12034a73c470481109a4d
Arebis <michal.sibera@windowslive.com>
2018-08-26 14:37:37 +0200

    Removed httpservice added rabbitmqService

---
+++ ARDroneBlockProgramming.Drone/src/rabbitMqService.ts
@@ -0,0 +1,14 @@
+import { DroneOperator } from './droneOperator';
+import { DroneAction } from './classes/droneAction';
+import { connect } from 'amqplib';
+
+
+export class RabbitMqService {
+    public constructor() {
+
+    }
+
+    public async initRabbitMq() {
+
+    }
+}
